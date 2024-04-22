1c086026d17ceb00c7ba3a72e7cffbfaadf69661
Raymond-Moody <144750662+Raymond-Moody@users.noreply.github.com>
2024-04-22 01:29:23 -0400

    Added Summary and Transcript to /latest

--- src/news/api.py
+++ src/news/api.py
@@ -2,6 +2,7 @@ from ninja import NinjaAPI, Schema
 from .models import Video
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -27,8 +28,8 @@ def latest(request):
 @api.get("/summarize")
 async def summarize(request):
     """
-    Checks for videos with blank summaries
-    Creates a summary if necessary, and store embeddings of the transcripts for later use
+    Checks for videos with blank summaries and creates a summary if necessary
+    Returns a dictionary of summaries in the format {video_id : summary}
     """
 
     # Create the langchain chain
@@ -51,6 +52,62 @@ async def summarize(request):
             await video.asave()
         summaries[video.id] = video.summary
     return summaries
+
+@api.get("/summary_html/{video_id}")
+def summary_html(request, video_id: str):
+    """
+    Returns the transcript and summary for a given video as html for the /latest template to render
+    """
+    response = HttpResponse()
+    try:
+        video = Video.objects.get(pk=video_id)
+        with open(video.transcript, "r") as f:
+            transcript = f.read()
+        transcript = transcript.replace("\n"," ")
+        response.write('<div class="row row-deck">')
+        response.write(
+            f"""
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-status-top bg-blue"></div>
+                    <div class="card-body">
+                        <h3 class="card-title">Transcript</h3>
+                        <p class="text-secondary">{transcript}</p>
+                    </div>
+                </div>
+            </div>
+            """
+        )
+        response.write(
+            f"""
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-status-top bg-green"></div>
+                    <div class="card-body">
+                        <h3 class="card-title">Summary</h3>
+                        <p class="text-secondary">{video.summary}</p>
+                    </div>
+                </div>
+            </div>
+            """
+        )
+        response.write("</div>")
+    except ObjectDoesNotExist:
+        response.write(f"Video with id {video_id} does not exist")
+
+    print(response)
+    return response
+
+@api.get("/title/{video_id}")
+def title(request, video_id: str):
+    """
+    Returns the title of the given video. Used to update the summary modal's title
+    """
+    try:
+        video = Video.objects.get(pk=video_id)
+        return HttpResponse(video.title)
+    except ObjectDoesNotExist:
+        return HttpResponse(f"Video with id {video_id} does not exist")
 
 class ChatSchema(Schema):
     message: str = ""
