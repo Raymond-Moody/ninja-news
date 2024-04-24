ddeb85a24b5650641a88775748ab5602eeb45f2f
Raymond-Moody <144750662+Raymond-Moody@users.noreply.github.com>
2024-04-23 22:37:04 -0400

    Created task to add documets to vector database unsplit

--- src/news/tasks.py
+++ src/news/tasks.py
@@ -1,10 +1,13 @@
+import os
 import asyncio
 from celery import shared_task
 from .producer import push_videos
 from .consumer import pull_messages
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_postgres.vectorstores import PGVector
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from django.conf import settings
 from news.models import Video
 
@@ -46,3 +49,29 @@ def generate_summaries():
             asyncio.run(get_summary(video))
         summaries[video.id] = video.summary
     return summaries
+
+@shared_task
+def populate_pgvector():
+    """
+    Populate the vector database with the transcripts
+    This task should only be run once
+    """
+    vectorstore = PGVector(
+        embeddings=OpenAIEmbeddings(model="text-embedding-3-large", api_key=settings.OPENAI_API_KEY),
+        collection_name="news",
+        connection="postgresql+psycopg://langchain:langchain@pgvector:5432/langchain",
+        use_jsonb=True,
+    ) 
+
+    # Just in case this task was run before, drop tables to prevent inserting videos twice
+    #vectorstore.drop_tables()
+
+    # Create document loader for transcript directory
+    transcript_path = os.path.join(settings.BASE_DIR, "transcripts/")
+    loader = DirectoryLoader(transcript_path, loader_cls=TextLoader)
+    docs = loader.load()
+
+    vectorstore.add_documents(docs)
+
+    #Test the vectors
+    print(vectorstore.similarity_search("Ukraine"))
